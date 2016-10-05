716468e34ee754de93aa7e4a4f3728e2cf6a8257
Scordevil <Cardenasg66@gmail.com>
2016-10-05 13:15:40 -0500

    ajuste de campos requeridos

--- src/java/com/sv/controladores/VotacionCT.java
+++ src/java/com/sv/controladores/VotacionCT.java
@@ -11,6 +11,7 @@ import com.sv.dao.VotacionDao;
 import com.sv.modelos.Comite;
 import com.sv.modelos.Inventario;
 import com.sv.modelos.Votacion;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -61,39 +62,63 @@ public class VotacionCT {
     }
 
     public void onrate(RateEvent rateEvent) {
-        votacion = new Votacion();
-        votacion.setIdInventario(juguete);
-        votacion.setIdUsuario(Sesion.obtenerSesion());
-        votacion.setFechaCalificacion(new Date());
+//        votacion = new Votacion();
+//        votacion.getIdInventario().setIdInventario(juguete.getIdInventario());
+//        votacion.setIdUsuario(Sesion.obtenerSesion());
+//        votacion.setFechaCalificacion(new Date());
         calificacion = Integer.parseInt(rateEvent.getRating().toString());
-        votacion.setCalificacion("" + calificacion);
-        ComiteDao comiteDao = new ComiteDao();
-        votacion.setIdComite(new Comite(comiteDao.consultarComitePorUsuario(votacion.getIdUsuario().getIdUsuario())));
-
-        boolean existe = false;
-        for (int i = 0; i < votaciones.size(); i++) {
-            if (votaciones.get(i).getIdInventario().getIdInventario().equals(votacion.getIdInventario())) {
-                existe = true;
-            }
-        }
-        if (!existe) {
-            votaciones.add(votacion);
-        } else {
-            for (int i = 0; i < votaciones.size(); i++) {
-                if (votaciones.get(i).getIdInventario().getIdInventario().equals(votacion.getIdInventario())) {
-                    votaciones.set(i, votacion);
-                }
-            }
-        }
+//        votacion.setCalificacion("" + calificacion);
+//        ComiteDao comiteDao = new ComiteDao();
+//        votacion.setIdComite(new Comite(comiteDao.consultarComitePorUsuario(votacion.getIdUsuario().getIdUsuario())));
+//
+//        boolean existe = false;
+//        for (int i = 0; i < votaciones.size(); i++) {
+//            if (votaciones.get(i).getIdInventario().getIdInventario().equals(votacion.getIdInventario())) {
+//                existe = true;
+//            }
+//        }
+//        if (!existe) {
+//            votaciones.add(votacion);
+//        } else {
+//            for (int i = 0; i < votaciones.size(); i++) {
+//                if (votaciones.get(i).getIdInventario().getIdInventario().equals(votacion.getIdInventario())) {
+//                    votaciones.set(i, votacion);
+//                }
+//            }
+//        }
     }
 
-    //Metodos
-    public void registrarVotaciones() {
+//    //Metodos
+//    public void registrarVotaciones() {
+//        VotacionDao votacionDao = new VotacionDao();
+//        int resultado = 0;
+//        for (int i = 0; i < votaciones.size(); i++) {
+//            resultado = votacionDao.registrarVotacion(votaciones.get(i));
+//        }
+//
+//        if (resultado == 1) {
+//            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Exito", "Votacion Registrada");
+//            FacesContext.getCurrentInstance().addMessage(null, message);
+//        } else if (resultado == 0) {
+//            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error", "Ocurrio algun problema en el proceso de votacion");
+//            FacesContext.getCurrentInstance().addMessage(null, message);
+//        }
+//        votaciones = new ArrayList<>();
+//        calificacion = 0;
+//    }
+//    //Metodos
+    public void registrarVotaciones(int idUsuario, int idInventario) {
         VotacionDao votacionDao = new VotacionDao();
         int resultado = 0;
-        for (int i = 0; i < votaciones.size(); i++) {
-            resultado = votacionDao.registrarVotacion(votaciones.get(i));
-        }
+        Votacion vota = new Votacion();
+        vota.setCalificacion(calificacion+"");
+        vota.getIdInventario().setIdInventario(idInventario);
+        vota.getIdUsuario().setIdUsuario(idUsuario);
+        ComiteDao comiteDao = new ComiteDao();
+        vota.setIdComite(new Comite(comiteDao.consultarComitePorUsuario(idUsuario)));
+        vota.setFechaCalificacion(new Date());
+
+        resultado = votacionDao.registrarVotacion(vota);
 
         if (resultado == 1) {
             FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Exito", "Votacion Registrada");
@@ -103,7 +128,7 @@ public class VotacionCT {
             FacesContext.getCurrentInstance().addMessage(null, message);
         }
         votaciones = new ArrayList<>();
-        calificacion = 0;
+//        calificacion = 0;
     }
 
 }
